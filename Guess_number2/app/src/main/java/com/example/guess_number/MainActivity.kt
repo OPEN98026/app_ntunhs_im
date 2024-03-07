33199da8991fffc33f5510d8dba29c217500b033
OPEN98026 <open98026@gmail.com>
2024-03-07 18:09:33 +0800

    update

--- Guess_number2/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number2/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.guess_number
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -8,51 +10,56 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
+        handler = Handler(Looper.getMainLooper())
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView)
         val guess_button = findViewById<Button>(R.id.GUESS)
         val reset_button = findViewById<Button>(R.id.RESET)
-        //val ANS_button = findViewById<Button>(R.id.ANS)
         val editText = findViewById<EditText>(R.id.editTextText)
-        var validate_num:Int
-        var str:Int = 1
-        var end:Int = 100
+        var validate_num: Int
+        var str: Int = 1
+        var end: Int = 100
 
-        var secret : Int = Random().nextInt(100)+1 //確保生出來的數值正確階為設定值 1-10
-
+        var secret: Int = Random().nextInt(100) + 1
         guess_button.setOnClickListener {
-            //textView.text = editText.text
-            validate_num=editText.text.toString().toInt()-secret
-            var ans_str:String="你猜對了喔"
+            textView.text = editText.text
+            validate_num = editText.text.toString().toInt() - secret
+            var ans_str: String
 
-            if (validate_num>0){
+            if (validate_num > 0) {
                 end = editText.text.toString().toInt()
-                ans_str="$str ~ $end"
-            }else if (validate_num<0) {
+                ans_str = "$str ~ $end"
+            } else if (validate_num < 0) {
                 str = editText.text.toString().toInt()
-                ans_str="$str ~ $end"
+                ans_str = "$str ~ $end"
+            } else if (validate_num == 0) {
+                ans_str = "Yes, you got the answer"
+
+                handler.postDelayed({
+                    str = 1
+                    end = 100
+                    secret = Random().nextInt(100) + 1
+                    textView.text = "try again"
+                }, 5000)
+            } else {
+                ans_str = "" // 在validate_num不等於0的狀況設為""
             }
-            textView.text=ans_str
+            textView.text = ans_str
         }
 
-        reset_button.setOnClickListener{
+        reset_button.setOnClickListener {
             str = 1
             end = 100
-            secret = Random().nextInt(100)+1
-            textView.text="我們再猜一次"
+            secret = Random().nextInt(100) + 1
+            textView.text = "try again"
         }
-
-        //guess_button.setOnClickListener { //Toast之應用
-        //    Toast.makeText(this,editText.text,Toast.LENGTH_LONG)
-        //}
-
-        //guess_button.setOnClickListener{ //Toast-alter
-        //    Toast.makeText(this,editText.text,Toast.LENGTH_LONG).show()
-        //    AlterDialog.Builder(this).setTitle("onclick").setMessage("onclick").create().show()
-        //}
-
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
